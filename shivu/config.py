9989578ca6d1410c942222c85bd21290ff782e12
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-08-02 19:35:38 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,17 +3,17 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "7078181502"
-    sudo_users = "5884969921", "6961287189"
+    SUDO_USERS = "5884969921", "6961287189"
     GROUP_ID = -1002009280180
     TOKEN = "7070617268:AAHIktRsnSY1coCeYh32MFwTrbCGAyfgg3w"
-    mongo_url = "mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_URI = "mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/c9744c30554b46644a800.jpg", "https://telegra.ph/file/77fd10eeccd16912ccf5b.jpg"]
     SUPPORT_CHAT = "https://t.me/lolpagalokigc"
     UPDATE_CHAT = "https://t.me/mlohvdryj"
     BOT_USERNAME = "bulma_music_bot"
     CHARA_CHANNEL_ID = "-1002009280180"
-    api_id = 23255238
-    api_hash = "009e3d8c1bdc89d5387cdd8fd182ec15"
+    API_ID = 23255238
+    API_HASH = "009e3d8c1bdc89d5387cdd8fd182ec15"
 
     
 class Production(Config):
